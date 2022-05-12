8bb368c93e78681428b1e5063a7da0f083c457d2
kim-pink-ba <kimpinkba@gmail.com>
2022-05-12 17:18:02 +0800

    Add Slider

---
+++ packages/uikit/src/components/Slider/Slider.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import SliderProps from './types'
+import { Slider as MpSlider } from '@binance/mp-components'
+import { useTheme } from '@binance/mp-styled'
+
+const Slider: React.FC<SliderProps> = ({ min, max, value, onValueChanged, step, disabled = false, valueLabel }) => {
+  const theme = useTheme()
+  const onChange = (e) => {
+    if (onValueChanged) {
+      onValueChanged(e.detail.value)
+    }
+  }
+  const propStep = step === 'any' ? undefined : step
+  return (
+    <MpSlider
+      showValue={!!valueLabel}
+      activeColor={disabled ? '#a4a4a4' : theme.colors.primary}
+      onChange={onChange}
+      value={value}
+      min={min}
+      max={max}
+      step={propStep}
+      disabled={disabled}
+    />
+  )
+}
+
+export default Slider
